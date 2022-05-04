79f7ed90d3082fab0c295c2a9f710e808e6a638f
Godsfavour KIo <k.favour@pamo.app>
2022-05-04 09:23:38 +0000

    Initial Commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -39,7 +39,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             TextField(
-              onSubmitted: (String value) {
+              onChanged: (String value) {
                 city = value;
               },
               decoration: InputDecoration(
